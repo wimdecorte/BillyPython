68de80c965d6a52f238484ec4ab5e9a1d4deff01
Wim Decorte <wim@connectingdata.com>
2018-06-24 08:16:26 -0400

    log into FMS working

--- BillyPython/BillyPython.py
+++ BillyPython/BillyPython.py
@@ -1,1 +1,40 @@
+# import ptvsd
+import fmrest
+import time
+import atexit
+import os
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+import configparser
 
+# get the config file
+myfile = __file__
+mydir = os.path.dirname(myfile)
+Config = configparser.ConfigParser()
+Config.read(os.path.join(mydir, 'billy.ini'))
+
+# ptvsd.enable_attach(secret='1803')
+
+# recommended for auto-disabling motors on shutdown
+def turnOffMotors():
+	mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+# and to make sure it runs when quitting the script
+atexit.register(turnOffMotors)
+
+# make the connection to the FMS Data API
+fms = fmrest.Server(Config.get('FMS', 'url'),
+                        user=Config.get('FMS', 'user'),
+                        password=Config.get('FMS', 'pw'),
+                        database=Config.get('FMS', 'file'),
+                        layout=Config.get('FMS', 'layout'))
+
+token = fms.login()
+print(token)
+
+mh = Adafruit_MotorHAT(addr=0x60)
+myMotor = mh.getMotor(2)
+
+myMotor.setSpeed(150)
