94e410f3d0a29311f1216f12dd56001d4b4f7397
Wim Decorte <wim@connectingdata.com>
2018-06-29 10:14:29 -0400

    started on parsing the Polly viseme data

--- BillyPython/BillyPython.py
+++ BillyPython/BillyPython.py
@@ -10,6 +10,7 @@ import configparser
 import multiprocessing
 from multiprocessing import Process
 from omxplayer.player import OMXPlayer
+import json
 
 MOTOR_HEAD_TAIL = 1
 MOTOR_MOUTH = 2
@@ -34,11 +35,11 @@ def turnOffMotors():
 atexit.register(turnOffMotors)
 
 # function to tilt head up when the fish talks
-def head_tilt():
+def head_tilt(how_many_seconds):
     while True:
         head.run(Adafruit_MotorHAT.BACKWARD)
         # set to forward to move the tail
-        time.sleep(5)
+        time.sleep(how_many_seconds)
         head.run(Adafruit_MotorHAT.RELEASE)
 
 # function to tilt head up when the fish talks
@@ -54,6 +55,12 @@ def play_voice():
     player = OMXPlayer('play.mp3')
     player.set_volume(400)
     time.sleep(player.duration() + 1)
+
+def get_file(response):
+    with open('play.mp3', 'wb') as file_:
+        for chunk in response.iter_content(chunk_size=2048): 
+            if chunk:
+                file_.write(chunk)
 
 # make the connection to the FMS Data API
 fms = fmrest.Server(Config.get('FMS', 'url'),
@@ -104,11 +111,49 @@ print(str( datetime.now()) +' - mp3 is at ' + todo.audio_file)
 # download the mp3
 print(str( datetime.now()) +' - Downloading the mp3...')
 name, type_, length, response = fms.fetch_file(todo.audio_file, stream=True)
-with open('play.mp3', 'wb') as file_:
-    for chunk in response.iter_content(chunk_size=2048): 
-        if chunk:
-            file_.write(chunk)
 
+dl = Process(target=get_file, args=(response,))
+dl.start()
+
+# turn billy's head here
+print(str( datetime.now()) +' - Turning the head...')
+mouth_process = Process(target=head_tilt, args=(5,))
+mouth_process.start()
+
+#for x in range(0,4):
+#    print(str( datetime.now()) + ' - Message during download')
+#    time.sleep(1)
+
+# while file is downloading, process the visemes
+# keep only those that move the mouth
+# start at each word should close the mouth
+viseme_list = todo.audio_extra_info
+
+
+# --> how to determine amount of openess of the mouth?
+# percentage perhaps?  so that we can set the frequency? speed?
+
+
+# create empty dict
+dict = {}
+for line in viseme_list.splitlines():
+    print(str( datetime.now()) + ' ' + line)
+    json_line = json.loads(line)
+    # time, type and value
+    when = json_line['time']
+    what = json_line['type']
+    vis = json_line['value']
+    if vis == 'sil':
+        dict[ str(when)] = 'close'
+    # value = sil for silent, time to release motor
+    # store in what variable for reading during playback?
+    # also need to figure out how to calculate elapsed since playback start
+
+# make sure we wait for the download to finish
+dl.join()
+print(str( datetime.now()) + ' - Done downloading the mp3')
+
+# now play the audio and do the magic
 voice = Process(target=play_voice)
 print(str( datetime.now()) + ' - Start the playback')
 voice.start()
