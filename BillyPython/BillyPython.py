e013f6671009dc00b0c7e81a1e9273f59ccdc211
Wim Decorte <wim@connectingdata.com>
2018-07-17 11:10:02 -0400

    cleanup of unused code

--- BillyPython/BillyPython.py
+++ BillyPython/BillyPython.py
@@ -11,7 +11,6 @@ import multiprocessing
 from multiprocessing import Process
 from omxplayer.player import OMXPlayer
 import json
-import pyaudio
 import threading
 
 MOTOR_HEAD_TAIL = 1
@@ -60,43 +59,10 @@ def waggle_tail():
         head.run(Adafruit_MotorHAT.RELEASE)
         time.sleep(0.20)
 
-def mouth_open(how_many_seconds, how_wide):
-    print(str( datetime.now()) + ' - mouth movement for ' + str(how_many_seconds) + ' seconds, motor speed = ' + str(fish_mouth_speed))
-    if how_wide == 'full':
-        speed = fish_mouth_speed
-    elif how_wide == 'half':
-        speed = int(fish_mouth_speed / 2)
-    mouth.setSpeed(speed)
-    mouth.run(Adafruit_MotorHAT.BACKWARD)
-    time.sleep(float(how_many_seconds))
-    mouth.run(Adafruit_MotorHAT.RELEASE)
-    print(str( datetime.now()) + ' - mouth movement done')
-
 def play_voice():
     player = OMXPlayer('play.' + audio_type)
     player.set_volume(100)
     time.sleep(player.duration() + 1)
-
-def play_voice_in_sync():
-    player = OMXPlayer('play.' + audio_type)
-    #player.set_volume(100)
-    # p = pyaudio.PyAudio()
-    #stream = p.open(
-    #        format=p.get_format_from_width(2),
-    #        channels=1,
-    #        rate=16000,
-    #        output=True)
-    th = threading.Thread(target=handle_viseme, args=(viseme_data,))
-    th.start()
-    # time.sleep(player.duration() + 1)
-    #with open('play.WAV', 'rb') as pcm_file:
-    #    data = pcm_file.read(1024)
-    #    while data != '':
-    #        stream.write(data)
-    #        data = pcm_file.read(1024)
-    th.join()
-    #stream.stop_stream()
-    #stream.close()
 
 def handle_viseme(args):
     viseme_data = args
@@ -119,6 +85,8 @@ def handle_viseme(args):
 
 def get_file(response):
     print(str( datetime.now()) + ' - sub-process download started.')
+    name, type_, length, response = fms.fetch_file(todo.audio_file, stream=True)
+    print(str( datetime.now()) +' - fetched audio file details.')
     with open('play.' + audio_type, 'wb') as file_:
         for chunk in response.iter_content(chunk_size=2048): 
             if chunk:
@@ -167,150 +135,115 @@ head.setSpeed(fish_head_speed)
 
 # while True:
 print(str( datetime.now()) + ' - Starting the loop...')
+while True:
 
-# release both motors
-mouth.run(Adafruit_MotorHAT.RELEASE)
-head.run(Adafruit_MotorHAT.RELEASE)
+    # release both motors
+    mouth.run(Adafruit_MotorHAT.RELEASE)
+    head.run(Adafruit_MotorHAT.RELEASE)
 
-# first try to find if we have a request to just turn the head
+    # first try to find if we have a request to just turn the head
+    foundset = None
+    find_request = [{'flag_turn_head': '1'}]
+    try:
+        foundset = fms.find(query=find_request)
+    except FileMakerError:
+        if fms.last_error == 0:
+             if foundset is not None:
+                todo = foundset[0]
+                # override whatever was set in the config file for head movement
+                temp_boolean = fish_move_head
+                fish_move_head = True
+                print(str( datetime.now()) + ' - Head turning action requested...')
+                head_process = Process(target=head_tilt, args=(1,))
+                print(str( datetime.now()) +' - Turning the head...')
+                head_process.start()
+                print(str( datetime.now()) + ' - Updating the FM record')
+                todo['flag_turn_head'] = ''
+                fms.edit(todo)
+                # reset the configured head movement
+                fish_move_head = temp_boolean
+                print(str( datetime.now()) + ' ----------------------------------------------------------------------')
+                # break and continue the loop, we're not going to play any audio
+                break
+        if fms.last_error == 401:
+            # no problem, we're going to look for audio to play
+            print(str( datetime.now()) + ' - No head turning action requested...')
+        else:
+            print(str( datetime.now()) +' - Unexpected FMS error: ' + fms.last_error)
+            exit()
 
-foundset = None
-find_request = [{'flag_turn_head': '1'}]
-try:
-    foundset = fms.find(query=find_request)
-except FileMakerError:
-    if fms.last_error == 401:
-        # no problem, we're going to look for audio to play
-        print(str( datetime.now()) + ' - No head turning action requested...')
-    else:
-        print(str( datetime.now()) +' - Unexpected FMS error: ' + fms.last_error)
-        exit()
-
-
-if foundset is not None:
+    # find the todo records
+    find_request = [{'flag_ready': '1'}]
+    try:
+        foundset = fms.find(query=find_request)
+    except FileMakerError:
+        if fms.last_error == 401:
+            print(str( datetime.now()) + ' - No speech playback requests found...')
+            print(str( datetime.now()) + ' ----------------------------------------------------------------------')
+            # break and continue the loop
+            break
+        else:
+            print(str( datetime.now()) +' - Unexpected FMS error: ' + fms.last_error)
+            exit()
+ 
+    # we have something to play
+    print(str( datetime.now()) + ' - Record found...')
     todo = foundset[0]
-    # override whatever was set in the config file for head movement
-    temp_boolean = fish_move_head
-    fish_move_head = True
-    print(str( datetime.now()) + ' - Head turning action requested...')
-    head_process = Process(target=head_tilt, args=(1,))
+    old_notes = todo.notes
+    audio_type = todo.audio_type
+    print(str( datetime.now()) +' - audio file (' + audio_type + ') is at ' + todo.audio_file)
+
+    # prep the download process for the mp3
+    dl = Process(target=get_file, args=(response,))
+
+    # turn billy's head here
+    hhmmss =  todo.duration
+    if hhmmss == '':
+        duration_from_fm = 2
+    else:
+        [hours, minutes, seconds] = [int(x) for x in hhmmss.split(':')]
+        x = timedelta(hours=hours, minutes=minutes, seconds=seconds)
+        duration_from_fm = x.seconds
+    print(str( datetime.now()) +' - ' + audio_type + ' duration from FM = ' + str(duration_from_fm) + ' seconds')
+
+    head_process = Process(target=head_tilt, args=(duration_from_fm,))
     print(str( datetime.now()) +' - Turning the head...')
     head_process.start()
-    print(str( datetime.now()) + ' - Updating the FM record')
-    todo['flag_turn_head'] = ''
-    fms.edit(todo)
-    # reset the configured head movement
-    fish_move_head = temp_boolean
-    print(str( datetime.now()) + ' ----------------------------------------------------------------------')
-    # break and continue the loop
-    exit()
+
+    print(str( datetime.now()) +' - Downloading the ' + audio_type + '...')
+    dl.start()
+
+    # while file is downloading, process the visemes
+    # keep only those that move the mouth
+    # start at each word should close the mouth
+    viseme_list = todo.audio_extra_info
 
 
-# find the todo records
-find_request = [{'flag_ready': '1'}]
-try:
-    foundset = fms.find(query=find_request)
-except FileMakerError:
-    if fms.last_error == 401:
-        print(str( datetime.now()) + ' - No speech playback requests found...')
-        print(str( datetime.now()) + ' ----------------------------------------------------------------------')
-        # break and continue the loop
-        exit()
-    else:
-        print(str( datetime.now()) +' - Unexpected FMS error: ' + fms.last_error)
-        exit()
- 
+    # create empty list
+    viseme_data = []
+    for line in viseme_list.splitlines():
+        # print(str( datetime.now()) + ' ' + line)
+        json_line = json.loads(line)
+        # time, type and value
+        when = json_line['time']
+        what = json_line['type']
+        vis = json_line['value']
 
-print(str( datetime.now()) + ' - Record found...')
-todo = foundset[0]
-old_notes = todo.notes
-audio_type = todo.audio_type
-print(str( datetime.now()) +' - audio file (' + audio_type + ') is at ' + todo.audio_file)
+        if vis in vowels_open:
+            viseme_data.append([vis, when])
 
-# prep the download process for the mp3
-name, type_, length, response = fms.fetch_file(todo.audio_file, stream=True)
-print(str( datetime.now()) +' - fetched audio file details.')
-dl = Process(target=get_file, args=(response,))
+    print(str( datetime.now()) + ' - Done parsing the visemes')
 
+    # make sure we wait for the download to finish
+    dl.join()
+    print(str( datetime.now()) + ' - Done downloading the ' + audio_type)
 
-# turn billy's head here
-hhmmss =  todo.duration
-if hhmmss == '':
-    duration_from_fm = 2
-else:
-    [hours, minutes, seconds] = [int(x) for x in hhmmss.split(':')]
-    x = timedelta(hours=hours, minutes=minutes, seconds=seconds)
-    duration_from_fm = x.seconds
-print(str( datetime.now()) +' - ' + audio_type + ' duration from FM = ' + str(duration_from_fm) + ' seconds')
-
-head_process = Process(target=head_tilt, args=(duration_from_fm,))
-print(str( datetime.now()) +' - Turning the head...')
-head_process.start()
-print(str( datetime.now()) +' - Downloading the ' + audio_type + '...')
-dl.start()
-
-# while file is downloading, process the visemes
-# keep only those that move the mouth
-# start at each word should close the mouth
-viseme_list = todo.audio_extra_info
-
-
-# create empty list
-action_list = []
-viseme_data = []
-action_found = False
-for line in viseme_list.splitlines():
-    # print(str( datetime.now()) + ' ' + line)
-    json_line = json.loads(line)
-    # time, type and value
-    when = json_line['time']
-    what = json_line['type']
-    vis = json_line['value']
-
-    if vis in vowels_open:
-        viseme_data.append([vis, when])
-
-    # we are only going to look at vowels for now
-    # but need to record when the next one kicks in so that we have a duration
-
-    if action_found == True:
-        when_end = when
-        # add the action tuple to the list
-        action_list.append((action, when_start, when_end))
-
-    action_found = False
-    if vis == 'sil':
-        action = 'close'
-        action_found = True
-    elif vis in vowels_close:
-         action = 'half'
-         action_found = True
-    elif vis in vowels_open:
-        action = 'full'
-        action_found = True
-    elif vis in vowels_mid:
-        action = 'half'
-        action_found = True
-    elif vis in consonants:
-        action = 'half'
-        action_found = True
-
-    if action_found == True:
-        when_start = when
-print(str( datetime.now()) + ' - Done parsing the visemes')
-
-# make sure we wait for the download to finish
-dl.join()
-print(str( datetime.now()) + ' - Done downloading the ' + audio_type)
-
-# now play the audio and do the magic
-if audio_type == 'MP3':
+    # now play the audio and do the magic
     voice = Process(target=play_voice)
     th = threading.Thread(target=handle_viseme, args=(viseme_data,))
     print(str( datetime.now()) + ' - Start the mp3 playback')
-    #start_time = datetime.now() + timedelta(milliseconds=fish_mouth_wait)
-    #print(str( datetime.now()) + ' - Start marker time = ' + str(start_time))
     voice.start()
+
     print(str( datetime.now()) + ' - Delaying the motor action by ' + str(fish_mouth_wait) + ' milliseconds')
     time.sleep(fish_mouth_wait / 1000.0)
     th.start()
@@ -318,57 +251,16 @@ if audio_type == 'MP3':
     th.join()
     voice.join()
 
-    if False:
+    print(str( datetime.now()) + ' - Done with the motor action')
 
+    # now update the FM record to mark that it is done
+    print(str( datetime.now()) + ' - Updating the FM record')
+    todo['flag_ready'] = ''
+    todo['notes'] = 'Done - ' + str( datetime.now()) + '\n' + old_notes
+    fms.edit(todo)
+    print(str( datetime.now()) + ' - FM record updated')
+    head_process.join()
+    print(str( datetime.now()) + ' ----------------------------------------------------------------------')
 
-        print(str( datetime.now()) + ' - Start the motor action')
-        # move the mouth in sync with the speech
-        for action_tuple in action_list:
-            print(str( datetime.now()) + ' - action: ' + str(action_tuple))
-    
-            # break down the tuple
-            action, when_start, when_end = action_tuple
-
-            # how long does this action take?
-            elapsed_time = int(when_end) - int(when_start)
-            action_time = start_time + timedelta(milliseconds=int(when_start))
-            # print(str( datetime.now()) + ' - action length: ' + str(elapsed_time) + ' milliseconds, start target = ' + str(action_time))
-            duration_in_seconds = float(elapsed_time) / 1000.0
-
-            # loop and wait until it is our turn
-            sleep_time = 2.0 / 1000.0  # to make it seconds
-    
-            # print(str( datetime.now()) + ' - waiting for: ' + str(action_time))
-            while  datetime.now() < action_time:
-                # sleep until the time comes for this action
-                time.sleep(sleep_time)
-            # the time has come, we can now process the action
-            # print(str( datetime.now()) + ' - the time has come for the action')
-            if action == 'close':
-                # release the motor
-                # print(str( datetime.now()) + ' - closing mouth')
-                mouth.run(Adafruit_MotorHAT.RELEASE)
-                # break
-            elif action == 'half':
-                # send half power
-                mouth_open(duration_in_seconds, action)
-                # break
-            elif action == 'full':
-                # send full power
-                mouth_open(duration_in_seconds, action)
-                # break
-elif audio_type == 'WAV':
-    print(str( datetime.now()) + ' - Start the wav playback')
-    play_voice_in_sync()
-           
-print(str( datetime.now()) + ' - Done with the motor action')
-
-
-# now update the FM record to mark that it is done
-print(str( datetime.now()) + ' - Updating the FM record')
-todo['flag_ready'] = ''
-todo['notes'] = 'Done - ' + str( datetime.now()) + '\n' + old_notes
-fms.edit(todo)
-print(str( datetime.now()) + ' - FM record updated')
-head_process.join()
-print(str( datetime.now()) + ' ----------------------------------------------------------------------')
+    # wait for a little bit before repeating the loop
+    time.sleep(0.1)
